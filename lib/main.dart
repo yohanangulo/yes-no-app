87bc0fd9ac1debb8cce21c1c195d15669c48b98e
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2024-09-10 12:07:20 -0400

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Yes No App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton.tonal(
+            onPressed: () {},
+            child: const Text('click me'),
+          ),
+        ),
+      ),
+    );
+  }
+}
